f3003f21df1cea4f85045db7e7561fda355a505e
patricksilva1 <patricksilva5160@gmail.com>
2022-12-27 17:34:27 -0300

    aws

---
+++ AppUsersApi/src/main/java/dev/patricksilva/users/PostHandler.java
@@ -0,0 +1,47 @@
+package dev.patricksilva.users;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.Gson;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class PostHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+
+        String requestBody = input.getBody();
+
+        Gson gson = new Gson();
+
+        Map<String, String> userDetails = gson.fromJson(requestBody, Map.class);
+
+        userDetails.put("userId", UUID.randomUUID().toString());
+
+        // TODO: Process user details
+
+        
+        Map returnValue = new HashMap();
+        returnValue.put("firstName", userDetails.get("firstName"));
+        returnValue.put("lastName", userDetails.get("lastName"));
+        returnValue.put("userId", userDetails.get("userId"));
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.withStatusCode(200);
+        response.withBody(gson.toJson(returnValue, Map.class));
+
+        Map responseHeaders = new HashMap();
+        responseHeaders.put("Content-Type", "application/json");
+
+        response.withHeaders(responseHeaders);
+
+        return response;
+    }
+}
